c4470b0527b6c91e326f28f026a1033d1a2e8896
Davi <N4linkWiki>
2025-10-26 16:55:33 -0300

    feat: add redirect:Login if not session configured.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Session } from "inspector/promises";
 import { redirect } from "next/navigation";
 
 export default function Home() {
+  if (!Session) {
+    redirect("/Login");
+  }
+
   redirect("/dashboard");
 }
